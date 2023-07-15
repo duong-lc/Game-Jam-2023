01d16e17735c956488935b3060ed938de976f8a9
Griffin <51525281+duong-lc@users.noreply.github.com>
2023-07-15 17:49:13 +0700

    update

--- 2023-Game-Jam/Assets/Scripts/IPooledCore.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-
-public interface IPooledCore<out T>
-{
-    void Init(PooledObjectCallbackData data, Action<T> killAction);
-    IEnumerator RunRoutine();
-}
