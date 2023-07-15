c03feb73b9666b32ff2ae05ef9251a7e63dcc017
operatorprime21 <nguyenkhoin@gmail.com>
2023-07-16 02:39:29 +0700

    fuck parallax

--- 2023-Game-Jam/Assets/Scripts/UI/SlideScroller.cs
+++ 2023-Game-Jam/Assets/Scripts/UI/SlideScroller.cs
@@ -5,33 +5,18 @@ using System;
 
 public class SlideScroller : MonoBehaviour
 {
-    private float startTime;
-    [SerializeField] private bool _moving = false;
-    [SerializeField] private int curSlide = 0;
-
-    private Vector3 startPos;
-    private Vector3 curPos;
-    private Vector3 destPos;
+    private int curSlide = 0;
+    public List<GameObject> slides = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        startPos = this.transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (_moving)
-        {
-            float movingTime = (Time.time - startTime) * 10f;
-            //Debug.Log(movingTime);
-            this.transform.position = Vector3.Lerp(curPos, destPos, movingTime);
-            if (movingTime >= 1)
-            {
-                _moving = false;
-            }
-        }
     }
     public bool FindSlide(int change)
     {
@@ -40,7 +25,7 @@ public class SlideScroller : MonoBehaviour
 
         try
         {
-            temp = this.transform.Find("slide (" + slideFind + ")").gameObject;
+            temp = slides[slideFind].gameObject;
         }
         catch (Exception e)
         {
@@ -52,35 +37,40 @@ public class SlideScroller : MonoBehaviour
         }
         else return false;
     }
+
     public void ScrollLeft()
     {
-        if (!_moving && FindSlide(-1) == true)
+        if (FindSlide(-1) == true)
         {
             curSlide--;
-            curPos = this.transform.position;
-            destPos = new Vector3(this.transform.position.x + 500, this.transform.position.y, this.transform.position.z);
-           // Debug.Log(destPos);
-            _moving = true;
-            startTime = Time.time;
+            foreach(GameObject slide in slides)
+            {
+                slide.SetActive(false);
+            }
+            slides[curSlide].SetActive(true);
         }
     }
 
     public void ScrollRight()
     {
-        if (!_moving && FindSlide(1) == true)
+        if (FindSlide(1) == true)
         {
             curSlide++;
-            curPos = this.transform.position;
-            destPos = new Vector3(this.transform.position.x - 500, this.transform.position.y, this.transform.position.z);
-            //Debug.Log(destPos);
-            _moving = true;
-            startTime = Time.time;
+            foreach (GameObject slide in slides)
+            {
+                slide.SetActive(false);
+            }
+            slides[curSlide].SetActive(true);
         }
     }
 
     public void ExitTutorial()
     {
-        this.transform.position = startPos;
         curSlide = 0;
+        foreach (GameObject slide in slides)
+        {
+            slide.SetActive(false);
+        }
+        slides[curSlide].SetActive(true);
     }
 }
